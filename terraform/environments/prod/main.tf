944a7e2e7c1974bcc73182e59d7d4faf7d51aaf7
nachii-007 <nachikethasm25@gmail.com>
2026-06-23 10:20:20 +0530

    refactor: format terraform code

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -34,12 +34,12 @@ module "auth0_infra" {
   auth0_client_id     = var.auth0_client_id
   auth0_client_secret = var.auth0_client_secret
 
-  auth0_domain        = var.auth0_domain
-  auth0_audience      = var.auth0_audience
+  auth0_domain   = var.auth0_domain
+  auth0_audience = var.auth0_audience
 
   auth0_m2m_client_id     = var.auth0_m2m_client_id
   auth0_m2m_client_secret = var.auth0_m2m_client_secret
 
   auth0_management_api_audience = var.auth0_management_api_audience
-  github_actions_public_key = var.github_actions_public_key
+  github_actions_public_key     = var.github_actions_public_key
 }
